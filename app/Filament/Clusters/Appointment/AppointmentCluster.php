068875c69b1b8de18ded038fffa16ed73d2e5aa9
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-05-10 20:17:52 -0400

    feat: add getWidgetData override to AppointmentCluster

--- app/Filament/Clusters/Appointment/AppointmentCluster.php
+++ app/Filament/Clusters/Appointment/AppointmentCluster.php
@@ -5,10 +5,19 @@ namespace Modules\Appointment\Filament\Clusters\Appointment;
 use BackedEnum;
 use Filament\Clusters\Cluster;
 use Filament\Support\Icons\Heroicon;
+use Override;
 
 class AppointmentCluster extends Cluster
 {
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedSquares2x2;
 
     protected static ?string $navigationLabel = 'Appointment';
+
+    #[Override]
+    public function getWidgetData(): array
+    {
+        return [
+            // DispensingQueue
+        ];
+    }
 }
