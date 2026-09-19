233a446ce0791860c454e5109d41ab5ec6270d02
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-09-19 19:36:48 +0000

    feat: place the appointments cluster in the Patient Care sidebar group

--- app/Filament/Clusters/Appointment/AppointmentCluster.php
+++ app/Filament/Clusters/Appointment/AppointmentCluster.php
@@ -5,13 +5,18 @@ namespace Modules\Appointment\Filament\Clusters\Appointment;
 use BackedEnum;
 use Filament\Clusters\Cluster;
 use Filament\Support\Icons\Heroicon;
+use Modules\Core\Enums\SidebarGroup;
 use Override;
 
 class AppointmentCluster extends Cluster
 {
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedSquares2x2;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedCalendarDays;
 
-    protected static ?string $navigationLabel = 'Appointment';
+    protected static string|\UnitEnum|null $navigationGroup = SidebarGroup::PatientCare;
+
+    protected static ?int $navigationSort = 20;
+
+    protected static ?string $navigationLabel = 'Appointments';
 
     #[Override]
     public function getWidgetData(): array
